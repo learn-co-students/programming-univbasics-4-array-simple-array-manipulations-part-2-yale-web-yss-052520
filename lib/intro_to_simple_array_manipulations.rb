bdbc4b6e5d4a93a65e877e6ce617cbdefeb3115d
Alexis Teh <wenhui.teh23@gmail.com>
2020-05-10 02:55:51 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+
+def using_concat (a1, a2)
+  a1.concat(a2)
+end 
+
+def using_insert (a, e)
+  a.insert(4, e)
+end 
+
+def using_uniq (a)
+  a.uniq 
+end 
+
+def using_flatten (a)
+  a.flatten 
+end 
+
+def using_delete (a, s)
+  a.delete(s)
+end 
+
+def using_delete_at (a, i)
+  a.delete_at(i)
+end 
